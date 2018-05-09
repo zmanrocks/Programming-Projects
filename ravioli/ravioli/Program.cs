e0d211d778f7cfbffc0a6909bb0738107e2a00a4
zmanrocks <lemonboywithgun@gmail.com>
2018-05-08 20:03:57 -0500

    Added actual console writes to interact with as a game. Nothing else

--- ravioli/ravioli/Program.cs
+++ ravioli/ravioli/Program.cs
@@ -27,7 +27,7 @@ namespace ravioli
             if (IsAlive)
             {
                 Health -= enemy.Damage;
-                if (Health < 0)
+                if (Health <= 0)
                 {
                     IsAlive = false;
                    
@@ -40,7 +40,7 @@ namespace ravioli
             if (enemy.IsAlive)
             {
                 enemy.Health -= Damage;
-                if (Health < 0)
+                if (Health <= 0)
                 {
                     IsAlive = false;
                 }
@@ -51,7 +51,7 @@ namespace ravioli
     public class Enemy
     {
 
-        private string Name;
+        public string Name;
         public int Health = 100;
         public int Damage;
         public string enemyMessages;
@@ -71,13 +71,42 @@ namespace ravioli
     {
         static void Main(string[] args)
         {
+
             Player playerOne = new Player("Zach", 100, 20);
             Enemy enemyOne = new Enemy("Trogg", 15, 5);
             Enemy enemyTwo = new Enemy("Dragon", 1000, 50);
-            playerOne.TakeDamageFromEnemy(enemyTwo);
-            playerOne.TakeDamageFromEnemy(enemyOne);
-            playerOne.DoDamageToEnemy(enemyOne);
-            playerOne.TakeDamageFromEnemy(enemyTwo);
+            string readConsole = "";
+          
+            Console.WriteLine("{0} appears! With a total of {1} health, he seems undefeatable to most, do you wish to fight?", enemyOne.Name, enemyOne.Health);
+            Console.WriteLine("\n Type Yes or No to continue....");
+            Console.ReadLine();
+            if (readConsole == "Yes") {
+                Console.WriteLine("\n Great! You take on this dangerous challenge....");
+                Console.WriteLine("\n What shall be your first move?");
+                Console.WriteLine("\n Your current options here are: Attack, Loot, or Hide. ");
+                Console.ReadLine();
+                while (playerOne.IsAlive && enemyOne.IsAlive)
+                {
+                    if (readConsole == "Attack")
+                    {
+                        Console.WriteLine("/n ATTACK YOU SAY? LET'S DO SOME DAMAGE!!!");
+                        playerOne.DoDamageToEnemy(enemyOne);
+                        Console.WriteLine("You've landed your first shot for {0} damage! Now that's a lot of damage. ({1} has {2} Health remaining....)", playerOne.Damage, enemyOne.Name, enemyOne.Health);
+                    }
+                    if (readConsole == "Loot")
+                    {
+
+                    }
+                    if (readConsole == "Hide")
+                    {
+                        
+                    }
+                        
+                    
+                
+}
+                
+                    }
             Console.ReadLine();
         }
     }
